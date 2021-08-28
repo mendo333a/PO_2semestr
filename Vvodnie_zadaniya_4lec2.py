8ad3263d4f564ce7bd35698b3fc29afb63d7bb4f
mendo333a <74526893+mendo333a@users.noreply.github.com>
2021-08-28 13:28:13 +0800

    Delete Vvodnie_zadaniya_4lec2.py

--- Vvodnie_zadaniya_4lec2.py
+++
@@ -1,6 +0,0 @@
-print("This is the program to calculate the percentage content of symbols G (guanine) and C (cytosine)")
-stroka =  input("Enter the string:  ")
-stroka_g = stroka.upper().count('g'.upper())
-stroka_c = stroka.upper().count('c'.upper())
-summa_gc = stroka_g + stroka_c
-print(summa_gc/len(stroka)*100)
